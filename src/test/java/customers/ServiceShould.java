606a54ee84ec10ba662de70867579575f0ef3c37
Carlos Chacin <cchacin@gmail.com>
2020-03-19 21:11:49 -0700

    Add verifications using list::add

--- src/test/java/customers/ServiceShould.java
+++ src/test/java/customers/ServiceShould.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,77 +17,89 @@ class ServiceShould extends Assertions {
     @DisplayName("store customer in db and cache")
     void store_customer_in_db_and_cache() throws Exception {
         // Given
-        final var customer = Customer.create(this.id, this.id.toString());
+        final var customer = createCustomer();
+        final var db = new ArrayList<Customer>();
+        final var cache = new ArrayList<Customer>();
 
         // When
         final var sut = new Service(
                 uuid -> Optional.empty(),
                 uuid -> Optional.empty(),
-                cust -> {
-                },
-                cust -> {
-                }
+                db::add,
+                cache::add
         );
 
         // Then
         sut.create(customer);
+        assertThat(db).contains(customer);
+        assertThat(cache).contains(customer);
     }
 
     @Test
     @DisplayName("return customer from db when not present in cache")
     void return_customer_from_db_when_not_present_in_cache() throws Exception {
         // Given
-        final var customer = Customer.create(this.id, this.id.toString());
+        final var customer = createCustomer();
+        final var db = new ArrayList<Customer>();
+        final var cache = new ArrayList<Customer>();
 
         // When
         final var sut = new Service(
                 uuid -> Optional.empty(),
                 uuid -> Optional.of(customer),
-                cust -> {
-                },
-                cust -> {
-                }
+                db::add,
+                cache::add
         );
 
         // Then
-        assertThat(sut.find(this.id)).contains(Customer.create(this.id, this.id.toString()));
+        assertThat(sut.find(this.id)).contains(createCustomer());
+        assertThat(db).isEmpty();
+        assertThat(cache).contains(customer);
+    }
+
+    private Customer createCustomer() {
+        return Customer.create(this.id, this.id.toString());
     }
 
     @Test
     @DisplayName("return customer from cache when present")
     void return_customer_from_cache_when_present() throws Exception {
         // Given
-        final var customer = Customer.create(this.id, this.id.toString());
+        final var customer = createCustomer();
+        final var db = new ArrayList<Customer>();
+        final var cache = new ArrayList<Customer>();
 
         // When
         final var sut = new Service(
                 uuid -> Optional.of(customer),
                 uuid -> Optional.empty(),
-                cust -> {
-                },
-                cust -> {
-                }
+                db::add,
+                cache::add
         );
 
         // Then
-        assertThat(sut.find(this.id)).contains(Customer.create(this.id, this.id.toString()));
+        assertThat(sut.find(this.id)).contains(createCustomer());
+        assertThat(cache).isEmpty();
+        assertThat(db).isEmpty();
     }
 
     @Test
     @DisplayName("return empty when present in cache/db")
     void returnEmpty() throws Exception {
         // Given
+        final var db = new ArrayList<Customer>();
+        final var cache = new ArrayList<Customer>();
 
         // When
         final var sut = new Service(
                 uuid -> Optional.empty(),
                 uuid -> Optional.empty(),
-                cust -> {
-                },
-                cust -> {
-                }
+                db::add,
+                cache::add
         );
         // Then
         assertThat(sut.find(this.id)).isEmpty();
+        assertThat(cache).isEmpty();
+        assertThat(db).isEmpty();
     }
 }
